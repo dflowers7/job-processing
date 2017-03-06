875c3c4ecaf70d3bc2209ee7f8b6ecbabbfa65c5
dflowers7 <dflowers@mit.edu>
2017-03-06 16:21:32 -0500

    Added JobFiles.m for obtaining file names

---
+++ JobFiles.m
@@ -0,0 +1,49 @@
+function [outfiles,outfiles_jis,infiles,infiles_jis,cmdfiles,cmdfiles_jis] = JobFiles(direc)
+% [outfiles,outfiles_jis,infiles,infiles_jis,cmdfiles,cmdfiles_jis] = JobFiles(direc)
+
+% Get input file names
+ninfiles = numel(dir(fullfile(direc, 'input*.mat')));
+infiles_nopath = arrayfun(@(i){sprintf('input%g.mat',i)}, (1:ninfiles)');
+infiles = fullfile(direc, infiles_nopath);
+infiles_jis = (1:ninfiles).'; % Job indices are in increasing order because I constructed them that way
+
+% Get all possible output file names
+outfiles_nopath = regexprep(infiles_nopath, 'in', 'out');
+outfiles = fullfile(direc, outfiles_nopath);
+outfiles_jis = infiles_jis;
+
+% Filter down to output files that exist
+outfileexists = cellfun(@(f)exist(f, 'file')>0, outfiles);
+outfiles = outfiles(outfileexists);
+noutfiles = numel(outfiles);
+outfiles_jis = outfiles_jis(outfileexists);
+
+% Get existing command line output files
+jobname = regexp(direc, '(?<=\d+\.\d+\.\d+-\d+\.\d+\_)[^\.]+', 'match');
+jobname = jobname{1};
+filepattern_else = [jobname '.o*'];
+cmdfiles = dir(fullfile(direc, filepattern_else));
+if isempty(cmdfiles)
+    filepattern_c3ddb = 'out_*.out';
+    cmdfiles = dir(fullfile(direc, filepattern_c3ddb));
+    isc3ddb = true;
+else
+    isc3ddb = false;
+end
+cmdfiles = fullfile(direc,{cmdfiles.name}');
+
+% Get job array indices corresponding to command line output files
+cmdfiles_jis = zeros(numel(cmdfiles),1);
+for i = 1:numel(cmdfiles)
+    txt = fileread(cmdfiles{i});
+    cmdoutfiles_ji = regexp(txt, 'Running job (\d+) in array', 'tokens');
+    cmdoutfiles_ji = cmdoutfiles_ji{1}{1};
+    cmdfiles_jis(i) = str2double(cmdoutfiles_ji);
+end
+
+% Sort command line output files by their job indices
+temp = sortrows(table(cmdfiles,cmdfiles_jis),'cmdfiles_jis');
+cmdfiles = temp{:,1};
+cmdfiles_jis = temp{:,2};
+
+end
